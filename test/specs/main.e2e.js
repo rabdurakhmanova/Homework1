9f7a58df730072197e139ef4bf154eedcad04013
rabdurakhmanova <87542357+rabdurakhmanova@users.noreply.github.com>
2021-08-30 03:17:05 +0500

    Delete test directory

--- test/specs/main.e2e.js
+++
@@ -1,9 +0,0 @@
-describe('Main Page', () => {
-    it('Verify sign up option', () => {
-        browser.url('/');
-
-        const SignUp = $$("//body[1]/div[1]/div[2]/div[1]/p[3]/a[1]"); //findElements
-
-        expect(SignUp).toHaveHref('https://amity.uz/admissionmicrosite/register.php');
-    });
-});
